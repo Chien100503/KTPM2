7ec4e951a88460c8188a7630cfdadf2dac2a53b6
GAU-DEPTRAI\Admin <chienho10052003@gmail.con>
2025-02-13 19:48:03 +0700

    first commit

--- nodejs-back-end/config/db.config.js
+++ nodejs-back-end/config/db.config.js
@@ -1,13 +1,18 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
+// Load biến môi trường từ file .env
 dotenv.config();
 
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/sport-shop";
+
 const dbconnect = mongoose
-  .connect(process.env.MONGODB_URI, {
+  .connect(MONGODB_URI, {
     serverSelectionTimeoutMS: 5000,
   })
   .then(() => console.log("MongoDB is connected!"))
   .catch((err) => {
-    console.log(err);
+    console.error("MongoDB connection error:", err);
   });
+
 export default dbconnect;
